d645320a6ab22b464eb4605bd29703c17302a88e
Anand Ramachandran <Anand.nv1998@gmail.com>
2017-07-11 22:42:24 +0530

    Bring up AIM OTA
    Signed-off-by: Anand Ramachandran <Anand.nv1998@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,43 +1,28 @@
 LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+ifneq ($(ANDROID_BUILD_TOP),)
+    $(shell cp -u $(ANDROID_BUILD_TOP)/ota_conf $(LOCAL_PATH)/app/src/main/assets/)
+endif
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := AimOTA
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+LOCAL_ASSET_DIR := $(LOCAL_PATH)/app/src/main/assets
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_STATIC_JAVA_LIBRARIES += libcwac
+
+include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libcwac:app/libs/cwac-wakeful-1.0.5.jar
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-design \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-cardview \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    org.cyanogenmod.platform.internal \
-    volley
-
-LOCAL_RESOURCE_DIR := \
-    $(TOP)/frameworks/support/design/res \
-    $(TOP)/frameworks/support/v7/appcompat/res \
-    $(TOP)/frameworks/support/v7/cardview/res \
-    $(TOP)/frameworks/support/v7/preference/res \
-    $(TOP)/frameworks/support/v7/recyclerview/res \
-    $(LOCAL_PATH)/res
-
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.design \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v7.recyclerview \
-    --extra-packages android.support.v7.cardview
-
-LOCAL_AAPT_FLAGS += --rename-manifest-package org.lineageos.updater
-
-LOCAL_PACKAGE_NAME := CMUpdater
-
-LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
-
-LOCAL_PRIVILEGED_MODULE := true
-
-include $(BUILD_PACKAGE)
+include $(BUILD_MULTI_PREBUILT)
---
+++ README.md
@@ -0,0 +1,4 @@
+AIM ROM OTA APPLICATON
+==============================
+![Freedomhub](https://github.com/Anandnv/shits/blob/3384c53822a861688a90764af3c71c2a6a600ba6/ezgif.com-video-to-gif%281%29.gif)
+
